fc28b219489977f4e7334e2f05b0ff23b7d3afb9
Justin-Fa <xiefahit@gmail.com>
2020-09-16 05:32:36 +0800

    Finish advanced_ruby_exercises

---
+++ software-engineering-foundations/advanced-ruby/justin_fa_advanced_methods_exercise/05_max_inject.rb
@@ -0,0 +1,16 @@
+# Write a method, max_inject(arr), that accepts any amount of numbers arguments and returns
+# the largest number. Solve this using the built-in inject.
+
+def max_inject(*arr)
+  array = *arr
+  array.inject do |acc, el|
+    if acc < el
+      el
+    else
+      acc
+    end
+  end
+end
+
+p max_inject(1, -4, 0, 7, 5)  # => 7
+p max_inject(30, 28, 18)      # => 30
