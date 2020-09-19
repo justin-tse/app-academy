8b8d042fd20377eeecc8b21da5a100c76b225e96
Justin-Fa <xiefahit@gmail.com>
2020-09-19 13:30:32 +0800

    Optimized the code of procs project

--- 02-software-engineering-foundations/blocks-procs/justin-fa-procs_project/lib/part_2.rb
+++ 02-software-engineering-foundations/blocks-procs/justin-fa-procs_project/lib/part_2.rb
@@ -4,16 +4,27 @@ def reverser(string, &prc)
   prc.call(reverse_string)
 end
 
+# Optimized code
+def reverser(string, &prc)
+  prc.call(string.reverse)
+end
+
 def word_changer(sentence, &prc)
   sentence.split.map { |word| prc.call(word) }.join(" ")
 end
 
-def greater_proc_value(num, proc_1, proc_2)
-  [num, proc_1.call(num), proc_2.call(num)].max
+def greater_proc_value(num, prc_1, prc_2)
+  [num, prc_1.call(num), prc_2.call(num)].max
 end
 
-def and_selector(array, proc_1, proc_2)
-  array.select { |ele| proc_1.call(ele) && proc_2.call(ele) }
+# Optimized code the meaning is "should return the greater result of
+# the two procs when each is passed the number"
+def greater_proc_value(num, prc_1, prc_2)
+  [prc_1.call(num), prc_2.call(num)].max
+end
+
+def and_selector(array, prc_1, prc_2)
+  array.select { |ele| prc_1.call(ele) && prc_2.call(ele) }
 end
 
 def alternating_mapper(array,  prc_1, prc_2)
