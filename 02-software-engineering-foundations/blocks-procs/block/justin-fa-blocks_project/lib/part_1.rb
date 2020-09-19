eace31d33c958e218f21dc8f1f0d1923ea669f96
Justin-Fa <xiefahit@gmail.com>
2020-09-19 08:43:34 +0800

    Optimized block project's code

--- 02-software-engineering-foundations/blocks-procs/block/justin-fa-blocks_project/lib/part_1.rb
+++ 02-software-engineering-foundations/blocks-procs/block/justin-fa-blocks_project/lib/part_1.rb
@@ -8,6 +8,11 @@ def reject_puppies(dog)
   dog.reject { |hash| hash["age"] <=2 }
 end
 
+# Optimized code use better name of variables
+def reject_puppies(dogs)
+  dogs.reject { |dog| dog["age"] <=2 }
+end
+
 def count_positive_subarrays(array)
   array.count { |subarray| subarray.sum > 0 }
 end
@@ -22,6 +27,23 @@ def aba_translate(string)
 
   new_str
 end
+
+# Optimized style of code 
+def aba_translate(word)
+  vowels = "aeiou"
+  new_word = ""
+
+  word.each_char do |char|
+    if vowels.include?(char)
+      new_word += char + "b" + char
+    else
+      new_word += char
+    end
+  end
+
+  new_word
+end
+
 
 def aba_array(array)
   array.map { |string| aba_translate(string) }
--- 02-software-engineering-foundations/blocks-procs/block/justin-fa-blocks_project/lib/part_2.rb
+++ 02-software-engineering-foundations/blocks-procs/block/justin-fa-blocks_project/lib/part_2.rb
@@ -9,6 +9,19 @@ def no_valid_url?(urls)
   urls.none? { |url| count >= 1} 
 end
 
+# Optimized code
+def no_valid_url?(urls)
+  valid_endings = [".com", ".net", ".io", ".org"]
+  urls.none? do |url|
+    valid_endings.any? { |valid_ending| url.end_with?(valid_ending) }
+  end
+end
+
 def any_passing_students?(students)
   students.any? { |student| student[:grades].sum / student[:grades].length >= 75 } 
+end
+
+# Optimized code
+def any_passing_students?(students)
+  students.any? { |student| student[:grades].sum * 1.0 / student[:grades].length  >= 75}
 end
