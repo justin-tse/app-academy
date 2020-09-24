752decbd670eb4b0f9027b126c02ba03dab7f093
Justin-Fa <xiefahit@gmail.com>
2020-09-24 16:33:02 +0800

    Finish, optimized the code and rename the directory

---
+++ 02-software-engineering-foundations/classes/justin-fa-dog_project/lib/dog.rb
@@ -0,0 +1,50 @@
+class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def age
+    @age
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def bark
+    if @age > 3
+      @bark.upcase
+    else
+      @bark.downcase
+    end
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
+
+  # def favorite_food?(food)
+  #   self.favorite_foods.each do |favorite_food| 
+  #     if favorite_food.downcase == food.downcase
+  #       return true
+  #     end
+  #   end
+  #   false
+  # end
+
+  def favorite_food?(food)
+    @favorite_foods.map(&:downcase).include?(food.downcase)
+  end
+end
