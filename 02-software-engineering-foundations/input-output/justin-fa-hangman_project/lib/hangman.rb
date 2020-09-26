ece156322ab56c99ef3b025f5f6d8288df1450ae
Justin-Fa <xiefahit@gmail.com>
2020-09-26 16:59:27 +0800

    Finish hangman project part 1

--- 02-software-engineering-foundations/input-output/justin-fa-hangman_project/lib/hangman.rb
+++ 02-software-engineering-foundations/input-output/justin-fa-hangman_project/lib/hangman.rb
@@ -1,4 +1,42 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  def self.random_word
+    @randowm_word = DICTIONARY.sample
+  end
 
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+  
+  def get_matching_indices(char)
+    array = []
+    @secret_word.each_char.with_index { |c, i| array << i if char == c }
+    array
+  end
+
+  def fill_indices(char, array)
+    get_matching_indices(char).each do |index|
+      @guess_word[index] = char
+    end
+    @guess_word
+  end
 end
