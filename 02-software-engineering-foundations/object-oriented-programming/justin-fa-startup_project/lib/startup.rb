a6eea3ee27b0398623562ee0ce36f6545ec3a70f
Justin-Fa <xiefahit@gmail.com>
2020-10-01 09:36:19 +0800

    Finish startup project part 3

--- 02-software-engineering-foundations/object-oriented-programming/justin-fa-startup_project/lib/startup.rb
+++ 02-software-engineering-foundations/object-oriented-programming/justin-fa-startup_project/lib/startup.rb
@@ -52,4 +52,24 @@ class Startup
   def payday
     @employees.each { |employee| self.pay_employee(employee) }
   end
+
+  def average_salary
+    sum = 0
+    @employees.each { |employee| sum += @salaries[employee.title]}
+    sum / size.to_f
+  end
+
+  def close
+    @employees = []
+    @funding = 0
+  end
+
+  def acquire(other_startup)
+    @funding += other_startup.funding
+    other_startup.salaries.each do |title, salary| 
+      @salaries[title] = salary if !@salaries.has_key?(title)
+    end
+    @employees += other_startup.employees
+    other_startup.close    
+  end
 end
