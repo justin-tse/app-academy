880bb8ad08cee60cf34ade4e3ebe7f60cd80b00b
Justin-Fa <xiefahit@gmail.com>
2020-10-03 15:36:22 +0800

    Finish mastermind project part 3

--- 02-software-engineering-foundations/object-oriented-programming/justin-fa-mastermind_project/lib/mastermind.rb
+++ 02-software-engineering-foundations/object-oriented-programming/justin-fa-mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(other_code)
+    puts other_code.num_exact_matches(@secret_code)
+    puts other_code.num_near_matches(@secret_code)
+  end
+
+  def ask_user_for_guess
+    p "Enter a code"
+    user_guess = Code.from_string(gets.chomp)
+   print_matches(user_guess)
+    p  @secret_code
+    user_guess == @secret_code
+  end
 end
