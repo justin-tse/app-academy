7f3f1d3db5fe6f3a90a28f6cacc99d2f973855cf
Justin-Fa <xiefahit@gmail.com>
2020-10-05 23:59:54 +0800

    Finish battleship project part 1

--- 02-software-engineering-foundations/object-oriented-programming/justin-fa-battleship_project/lib/board.rb
+++ 02-software-engineering-foundations/object-oriented-programming/justin-fa-battleship_project/lib/board.rb
@@ -1,3 +1,23 @@
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n, :N) }
+    @size = n * n
+  end
+
+  def [](position)
+    row, col = position
+    @grid[row][col]
+  end
+
+  def []=(position, val)
+    row, col = position
+    @grid[row][col] = val
+  end
+
+  def num_ships 
+    @grid.flatten.count { |ele| ele == :S } 
+  end
+
 end
