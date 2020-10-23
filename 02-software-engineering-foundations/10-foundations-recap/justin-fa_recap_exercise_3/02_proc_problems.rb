87fc60eee109e7462579593e55f10bd4b5d4f078
Justin-Fa <xiefahit@gmail.com>
2020-10-23 12:23:30 +0800

    Finish recap exercise 3: proc problems

--- 02-software-engineering-foundations/10-foundations-recap/justin-fa_recap_exercise_3/02_proc_problems.rb
+++ 02-software-engineering-foundations/10-foundations-recap/justin-fa_recap_exercise_3/02_proc_problems.rb
@@ -5,21 +5,44 @@
 # original string that return true when passed into the block. If no block is 
 # passed, then return the empty string. Do not use the built-in 
 # Array#select in your solution.
+class String
+  def select(&prc)
+    prc ||= nil
+    # self.map(&prc)
+    selected = ""
+    return "" if prc == nil
+    self.each_char do |char|
+      if prc.call(char)
+        selected << char
+      end
+  end
 
+  selected
+  end
+end
 # Examples
-"app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
-"HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
-"HELLOworld".select          # => ""
+p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
+p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
+p "HELLOworld".select          # => ""
 
 
 
-# String#map!
-# Extend the string class by defining a String#map! method that accepts a block. 
-# The method should modify the existing string by replacing every character with 
-# the result of calling the block, passing in the original character and it's 
-# index. Do not use the built-in Array#map or Array#map! in your solution.
+# # String#map!
+# # Extend the string class by defining a String#map! method that accepts a block. 
+# # The method should modify the existing string by replacing every character with 
+# # the result of calling the block, passing in the original character and it's 
+# # index. Do not use the built-in Array#map or Array#map! in your solution.
+class String
+  def map!(&prc)
+    self.each_char.with_index do |char, i|
+      self[i] = prc.call(char, i)
+    end
+  end
 
-# Examples
+end
+
+
+# # Examples
 word_1 = "Lovelace"
 word_1.map! do |ch| 
     if ch == 'e'
