9ff22c1964ee945c63ff373dc717b0d04c309a8d
Justin-Fa <xiefahit@gmail.com>
2020-10-27 09:58:45 +0800

    Initial nauseating numbers

---
+++ 02-software-engineering-foundations/11-additional-projects/justin-nasuseating_numbers/phase_1_no_big_deal.rb
@@ -0,0 +1,65 @@
+def strange_sums(array)
+    count = 0
+    (0...array.length).each do |i|
+        (i + 1...array.length).each do |j|
+            count += 1 if array[i] + array[j] == 0
+        end
+    end
+
+    count
+end
+
+# p strange_sums([2, -3, 3, 4, -2])     # 2
+# p strange_sums([42, 3, -1, -42])      # 1
+# p strange_sums([-5, 5])               # 1
+# p strange_sums([19, 6, -3, -20])      # 0
+# p strange_sums([9])                   # 0
+
+def pair_product(array, product)
+    (0...array.length).each do |i|
+        (i + 1...array.length).each do |j|
+            return true if array[i] * array[j] == product
+        end
+    end
+    false
+end
+
+# p pair_product([4, 2, 5, 8], 16)    # true
+# p pair_product([8, 1, 9, 3], 8)     # true
+# p pair_product([3, 4], 12)          # true
+# p pair_product([3, 4, 6, 2, 5], 12) # true
+# p pair_product([4, 2, 5, 7], 16)    # false
+# p pair_product([8, 4, 9, 3], 8)     # false
+# p pair_product([3], 12)             # false
+
+def rampant_repeats(str, hash)
+    (str.split("").map do |char| 
+        if hash.keys.include?(char)
+            char * hash[char] 
+        else
+            char
+        end
+    end).join("")
+end
+
+# p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+# p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+# p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+# p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
+
+def perfect_square(num)
+    (1..num).each do |i|
+        return true  if i == num / i.to_f
+    end
+    false
+end
+
+# p perfect_square(1)     # true
+# p perfect_square(4)     # true
+# p perfect_square(64)    # true
+# p perfect_square(100)   # true
+# p perfect_square(169)   # true
+# p perfect_square(2)     # false
+# p perfect_square(40)    # false
+# p perfect_square(32)    # false
+# p perfect_square(50)    # false
---
+++ 02-software-engineering-foundations/11-additional-projects/justin-nauseating_numbers.rb
@@ -0,0 +1,10 @@
+# https://open.appacademy.io/learn/full-stack-online/software-engineering-foundations/nauseating-numbers
+# Nauseating Numbers
+
+# Phase 1: No big deal.
+
+# Phase 2: Nothing you can't handle.
+
+# Phase 3: Now we're having fun.
+
+# Phase 4: Nauseating.
