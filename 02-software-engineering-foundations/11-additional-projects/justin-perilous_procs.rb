b5842433ac3eb5df568a06ba42b2301d9b832c54
Justin-Fa <xiefahit@gmail.com>
2020-10-28 17:47:59 +0800

    Initial perilous procs

---
+++ 02-software-engineering-foundations/11-additional-projects/justin-perilous_procs.rb
@@ -0,0 +1,11 @@
+# https://open.appacademy.io/learn/full-stack-online/software-engineering-foundations/perilous-procs
+
+# Perilous Procs
+
+# Phase 1: Problems that are pleasant.
+
+
+# Phase 2: The proc thickens.
+
+
+# Phase 3: Perilous.
