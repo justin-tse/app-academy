c3b56f3a54a8a0e03017f205348b61abe9510566
Justin-Fa <xiefahit@gmail.com>
2020-10-29 17:58:17 +0800

    Initial meddling medley

---
+++ 02-software-engineering-foundations/11-additional-projects/justin-meddling_medley.rb
@@ -0,0 +1,11 @@
+# https://open.appacademy.io/learn/full-stack-online/software-engineering-foundations/meddling-medley
+
+# Meddling Medley
+
+# Phase 1: Modest problems.
+
+
+
+
+# Phase 2: More difficult, maybe?
+
