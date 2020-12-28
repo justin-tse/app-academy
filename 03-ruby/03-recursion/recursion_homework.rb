21a05110ab203140ffcc0ad3ea76c63ffdf847aa
justin-tse <xiefahit@gmail.com>
2020-12-28 11:36:21 +0800

    Note: Ssep to write recursive functions

---
+++ 03-ruby/03-recursion/recursion_homework.rb
@@ -0,0 +1,6 @@
+# Step to write recursive functions
+# 1.Read the problem statement to make sure you fully understand the problem 
+# 2. Identify the base case(s) 
+# 3. Determine the inductive step 
+# 4. Write the function 
+# 5. Run the provided test cases to verify you have a working solution 
