9213b9183f3d4343a59ca4b4a2f3236ab94864a2
justin-tse <xiefahit@gmail.com>
2021-01-18 22:55:46 +0800

    Update tree_node

--- 03-ruby/05-data-structures/poly_tree_node/lib/00_tree_node.rb
+++ 03-ruby/05-data-structures/poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=
+    node2.parent = node1
+    node3.parent = node1
+  end
 
 end
